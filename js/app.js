6237e26ccb260d3ca9e1883e044fa209e88f7967
RicardoFerreira2020 <ricardo.informatica.2014@gmail.com>
2022-07-19 18:38:03 -0300

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let alternador = document.querySelector('.js-botao-chaveador');
+
+
+alternador.onclick = function() {
+     let menu = document.querySelector('.js-menu');
+     menu.classList.toggle('menu--exibindo');
+}
